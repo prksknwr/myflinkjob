13e3ce723587bc378204544a0f378b6f65ffe8cf
Prakash Kunwar <prakash.kunwar@zalando.de>
2022-05-09 14:18:22 +0200

    removed directory

--- myflinkjob/notifications-messages-kafka-consumer-service/src/main/java/com/myflinkjob/kafka/consumers/NotificationConsumer.java
+++
@@ -1,6 +0,0 @@
-package com.myflinkjob.kafka.consumers;
-
-public interface NotificationConsumer extends Runnable {
-
-    void subscribe(PollingStrategy strategy);
-}
--- myflinkjob/notifications-messages-kafka-consumer-service/src/main/java/com/myflinkjob/kafka/consumers/PollingStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.myflinkjob.kafka.consumers;
-
-public class PollingStrategy {
-    public boolean pollForever() {
-        return true;
-    }
-}
